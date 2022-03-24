73eda2926afb3623c9b3fb2b38ab69f70fe58490
C.O.SEO <co.seo@lomin.ai>
2022-03-25 08:22:37 +0900

    deploy

---
+++ _nuxt/static/1648164155/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftextscope-public-demo\u002F_nuxt\u002Fstatic\u002F1648164155",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftextscope-public-demo\u002F",assetsPath:"\u002Ftextscope-public-demo\u002F_nuxt\u002F",cdnURL:a}}}}(null));
